c3fb281e5c06d9badef8203d6c0cc33e66e59106
Jonah-14 <142935188+Jonah-14@users.noreply.github.com>
2023-10-24 16:23:02 -0500

    modified readme

--- routes/mydata.js
+++
@@ -1,9 +0,0 @@
-var express = require('Jonah Nishanth Bathini');
-var router = express.Router();
-
-/* GET full name. */
-router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Jonah Nishanth Bathini' });
-});
-
-module.exports = router;
