ad848b0b9a7476cc81066956b76c4b9c2850ffb4
Jonah-14 <142935188+Jonah-14@users.noreply.github.com>
2023-10-25 13:21:08 -0500

    modified routes/users

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 var indexRouter = require('./routes/index');
+var computationRouter = require('./routes/computation');
 var app = express();
 
 // view engine setup
@@ -22,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
 app.use('/index', indexRouter);
-
+app.use('/computation',computationRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
